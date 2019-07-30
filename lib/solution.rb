d957af5c94496138c66e6ce2c7c804ec547e4bd8
github username <github email address>
2019-07-30 10:27:25 +0100

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,13 @@
 def clock_angle(time)
-    # code your solution here
+    time = time.split(/:/)
+    hour = time[0].to_f
+    min = time[1].to_f
+    hour = ((hour * 30) + (min / 60) * 30)
+    min = min * 6
+    answer = hour-min.abs
+    if answer > 180
+        return (360 - answer)
+    else
+        answer
+    end
 end
